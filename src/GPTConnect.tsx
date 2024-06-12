2a7c11a99e3bc6651b92257a167a45900acd1f5e
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-12 00:17:21 -0500

    Add and config tailwindcss

--- src/GPTConnect.tsx
+++ src/GPTConnect.tsx
@@ -1,3 +1,7 @@
 export function GPTConnect() {
-  return <h1>GPTConnect</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-emerald-500 fade-in">
+      GPTConnect
+    </h1>
+  );
 }
